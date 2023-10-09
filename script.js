d2d02a1ed19e3798b7605349e387e875c667ea26
manu2458 <manuel.arias@stud.fhgr.ch>
2023-10-09 09:56:12 +0200

    Details & andere kleine Anpassungen
    stuff

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ import { supa } from "/supabase.js";
 
 console.log("00 JavaScript verbunden")
 
+function changeColor() {
+    var divElement = document.getElementById('level-uebersicht-box-1');
+    divElement.style.backgroundColor = yellow;
+}
 
-
-
+console.log("fuckoff")
