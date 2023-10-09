d617a86ab892c0a547d7090bf2b446366ae678c5
manu2458 <manuel.arias@stud.fhgr.ch>
2023-10-09 10:44:24 +0200

    post coaching lea

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ import { supa } from "/supabase.js";
 
 console.log("00 JavaScript verbunden")
 
-function changeColor() {
-    var divElement = document.getElementById('level-uebersicht-box-1');
-    divElement.style.backgroundColor = yellow;
-}
 
-console.log("fuckoff")
+var divElement = document.getElementById('level-uebersicht-box-1');
+divElement.addEventListener('click', changeColor)
+function changeColor() {
+    console.log(this)
+    divElement.style.backgroundColor = '#E4EF75';
+}
