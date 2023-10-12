750fa8471ea174d249da16057961b56f516ca2b9
whane98 <jermyn.doerig@gmx.ch>
2023-10-12 14:59:52 +0200

    getting more responsive websites, not finished

--- script.js
+++ script.js
@@ -56,3 +56,23 @@ levelUebersichtElements.forEach(function(element) {
         }
     });
     
+
+
+    // Select all elements with the class 'card-v2'
+var cardElements = document.querySelectorAll('.card-v2');
+
+// Function to handle card click
+function handleCardClick(event) {
+    // Reset the background color of all cards to the default
+    cardElements.forEach(function(card) {
+        card.style.backgroundColor = ''; // Reset background color to default
+    });
+
+    // Change the background color of the clicked card
+    event.currentTarget.style.backgroundColor = '#E4EF75';
+}
+
+// Add click event listeners to the cards
+cardElements.forEach(function(card) {
+    card.addEventListener('click', handleCardClick);
+});
