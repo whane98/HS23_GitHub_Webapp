cb4f64dddd978efef09e535638ec9c6d7b612a55
LinaWieland <119846854+LinaWieland@users.noreply.github.com>
2023-10-16 15:42:47 +0200

    button download

--- trainings.js
+++ trainings.js
@@ -2,16 +2,12 @@ import { supa } from "/supabase.js";
 
 console.log("00 JavaScript verbunden")
 
-
-// Button muss noch vom html implementiert werden //
-
-function toggleButton() {
-    const button = document.getElementById("button-training-starten");
-    if (button.innerHTML === "Training starten") {
-      button.innerHTML = "Training beenden";
-      button.style.backgroundColor = "red"; // Hintergrundfarbe auf Rot ändern
+function toggleTraining(button) {
+    if (button.textContent === "Training starten") {
+        button.textContent = "Training beenden";
+        button.style.backgroundColor = "red";
     } else {
-      button.innerHTML = "Training starten";
-      button.style.backgroundColor = ""; // Hintergrundfarbe zurücksetzen (leer lassen oder auf Ihre Standardfarbe setzen)
+        button.textContent = "Training starten";
+        button.style.backgroundColor = ""; // Zurücksetzen auf die Standard-Hintergrundfarbe
     }
-  }
+}
